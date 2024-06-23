17df2851ddc17adf8606376dd2b02059cd9774b6
Fernando Marino <fernandomarino858@gmail.com>
2024-06-23 01:55:47 -0300

    added cors config

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,12 @@ import "reflect-metadata";
 import "express-async-errors";
 import { initRoutes } from "./routes";
 import { handleGlobalErrors } from "./api/@shared/errors";
+import cors from "cors";
 
 
 export const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 initRoutes(app);
